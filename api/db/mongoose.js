3bea02d57b018607157acfdef540b7311babe409
kenchen <independentchen@gmail.com>
2020-11-24 17:29:21 +0800

    Tesing

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
-console.log(process.env.MONGO_URL);
-mongoose.connect(process.env.MONGO_URL, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-})
+// console.log(process.env.MONGO_URL);
+// mongoose.connect(process.env.MONGO_URL, {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// })
 
-mongoose.connection
- .once('open', () => console.log('Good to go!'))
- .on('error', (error) => {
- console.warn('Warning', error);
- });
+// mongoose.connection
+//  .once('open', () => console.log('Good to go!'))
+//  .on('error', (error) => {
+//  console.warn('Warning', error);
+//  });
