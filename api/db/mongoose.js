af69bee84a5e1a554aaa522c748828e2560248bc
kenchen <independentchen@gmail.com>
2020-11-24 14:00:39 +0800

    Fix: seperate mongo user:pwd and mongo url

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, {
   auth: {"authSource":"admin" },
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PWD,
+  useMongoClient: true,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
