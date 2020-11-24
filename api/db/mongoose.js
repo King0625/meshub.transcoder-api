0aaad7c0f065569963209098f0073134152526e7
kenchen <independentchen@gmail.com>
2020-11-24 16:28:03 +0800

    Fix: add auth key in mongoose config

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, {
+  auth:{authdb:"admin"},
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
