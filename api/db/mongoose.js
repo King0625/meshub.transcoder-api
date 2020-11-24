ed17be3116d8a23d51a31af286c7a6c7894eaaf2
kenchen <independentchen@gmail.com>
2020-11-24 17:02:20 +0800

    Fix: fix mongodb url

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, {
-  auth:{authdb:"admin"},
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
