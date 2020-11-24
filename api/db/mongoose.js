5e9aed324bfe8512079d6aa6076bbf447d3222aa
kenchen <independentchen@gmail.com>
2020-11-24 17:45:17 +0800

    Fix: use static url

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose')
 
-// console.log(process.env.MONGO_URL);
-// mongoose.connect(process.env.MONGO_URL, {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true
-// })
+mongoose.connect("mongodb://mesh-admin:mesh1234@allinone.meshub.tv:50128/webrtc-admin", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
 
-// mongoose.connection
-//  .once('open', () => console.log('Good to go!'))
-//  .on('error', (error) => {
-//  console.warn('Warning', error);
-//  });
+mongoose.connection
+ .once('open', () => console.log('Good to go!'))
+ .on('error', (error) => {
+ console.warn('Warning', error);
+ });
