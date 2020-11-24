03a79a9a2e84fb70a4419df464048fb214b7bd8b
kenchen <independentchen@gmail.com>
2020-11-24 14:07:13 +0800

    Fix: update mongo url config

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose')
 
 console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, {
-  auth: {"authSource":"admin" },
-  user: process.env.MONGO_USER,
-  pass: process.env.MONGO_PWD,
-  useMongoClient: true,
-  useNewUrlParser: true,
   useCreateIndex: true,
+  useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 })
