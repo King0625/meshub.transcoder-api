f5dcfcbfa429365da68f338734068215f053c811
kenchen <independentchen@gmail.com>
2021-08-04 10:54:09 +0800

    Feat: set default timezone to Asia/Taipei

--- api/models/job.js
+++ api/models/job.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Taipei");
 
 const jobSchema = new mongoose.Schema({
   account: {
